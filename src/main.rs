e3aa8f8240a9577eac5cb4cc22b63915e928de26
NanoStealth88 <chandler.bartholomew@gmail.com>
2022-12-16 22:55:15 -0600

    setting up a basic variable and printing it

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
 fn main() {
     
+     /* ---------------------------------
+                VARIABLES IN RUST
+     -----------------------------------*/
+
+    let x:i64 = 15;
+
+    println!("The value of the variable x = {}", x);
+
+
 }
