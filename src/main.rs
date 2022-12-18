23c45a4721cc76fbab4930559eb3ddbd550459ca
NanoStealth88 <chandler.bartholomew@gmail.com>
2022-12-17 22:01:35 -0600

    Updating comments related to mutable and immutable variables

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,12 @@ fn main() {
                 VARIABLES IN RUST
      -----------------------------------*/
 
-    let x:i64 = 15;
+    //let x:i64 = 15;     // variable is immutable and can only be a letter or an underscore.  This appears to be like a const in c++
+
+    let mut x:i64 = 15;     //this variable declaration allows this variable to be overwritten so this is like a normal variable in python or c++
 
     println!("The value of the variable x = {}", x);
 
+    x = 22;   
 
 }
